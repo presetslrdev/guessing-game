92edbf15d7af648201df20b745f1bd4645d45f14
Denis <presetslr@gmail.com>
2019-10-09 00:42:48 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange(minDigit, maxDigit) {
+        this.min = minDigit;
+        this.max = maxDigit;
+        console.log(this.min, this.max);
     }
 
     guess() {
-
+        return this.middle = Math.ceil((this.max + this.min)/2);
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
